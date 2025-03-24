f6d02596e3cae69023f472e8e98f6dd7681d43ca
Mateus Silva Araujo <mtsSAraujo@users.noreply.github.com>
2025-03-24 11:00:40 -0300

    refactor: mudança do path do request mapping

--- src/main/java/gov/fatec/manumanager/controller/interfaces/OSController.java
+++ src/main/java/gov/fatec/manumanager/controller/interfaces/OSController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/os")
+@RequestMapping("/api/equipament/os")
 public interface OSController {
 
     @PostMapping("/{equipamentId}")
